444901abcd65872a35c179365d1f4850fb478bcc
Raquel Fernandez <rmfernandezbermudez@gmail.com>
2025-02-07 12:21:16 +0100

    Añadidas funcionalidades al cerrar modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,47 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
-      <button className="show-modal">Show modal 1</button>
-      <button className="show-modal">Show modal 2</button>
-      <button className="show-modal">Show modal 3</button>
-      <div className="modal hidden">
-        <button className="close-modal">&times;</button>
-        <h1>I am a modal window 😍</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-      </div>
-      <div className="overlay hidden"></div>
+      <button className="show-modal" onClick={openModal}>
+        Show modal 1
+      </button>
+      <button className="show-modal" onClick={openModal}>
+        Show modal 2
+      </button>
+      <button className="show-modal" onClick={openModal}>
+        Show modal 3
+      </button>
+      {showModal && (
+        <>
+          <div className="modal">
+            <button className="close-modal" onClick={closeModal}>
+              &times;
+            </button>
+            <h1>I am a modal window 😍</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className="overlay" onClick={closeModal}></div>
+        </>
+      )}
     </>
   );
 }
